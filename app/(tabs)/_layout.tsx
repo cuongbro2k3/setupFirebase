07e54da9439034810aa2c8e805ec3b126bec6843
cuongbro2k3 <cuongid241@gmail.com>
2025-04-20 13:33:08 +0700

    Hướng dẫn tích hợp FireBase with React Native Expo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,8 @@
-import { Slot } from 'expo-router';
-import { AuthProvider } from '@/context/AuthContext';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <Slot />
-    </AuthProvider>
+    <Stack screenOptions={{headerShown: false}}>
+    </Stack>
   );
 }
